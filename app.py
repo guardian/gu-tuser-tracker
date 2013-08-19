6c13ac43cb2cee2f27327f1c5b687a66ddb6d497
Robert Rees <robert.rees@guardian.co.uk>
2013-08-19 21:08:00 +0100

    Creates a working display of data

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import logging
 from urllib import quote, urlencode
 from google.appengine.api import urlfetch
 
+from models import TrackedUser, Followers
+
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")))
 
@@ -17,5 +19,23 @@ class MainPage(webapp2.RequestHandler):
 
 		self.response.out.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+class TrackedUserList(webapp2.RequestHandler):
+	def get(self):
+		template = jinja_environment.get_template('user-listing.html')
+
+		template_values = {'tracked_users' : TrackedUser.query().order(TrackedUser.username)}
+
+		self.response.out.write(template.render(template_values))
+
+class UserDetails(webapp2.RequestHandler):
+	def get(self, username):
+		template = jinja_environment.get_template('user-data.html')
+
+		template_values = {'follower_data' : Followers.query(Followers.username == username).order(-Followers.when)}
+
+		self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', MainPage),
+	('/tracked-users', TrackedUserList),
+	('/user/(.*)', UserDetails)],
                               debug=True)
